4c2cfd8a67390e0398c5072f901b6c788548a1a9
Piotr Stefanowicz <stefan8357@gmail.com>
2017-10-09 21:12:54 +0200

    MathEquation/displaying equation on the board

--- nastka-app/app/src/components/Math/MathEquation.js
+++ nastka-app/app/src/components/Math/MathEquation.js
@@ -1,10 +1,14 @@
 import React from 'react'
+import getRandomElement from '../../_utils/getRandomElement/getRandomElement'
 
 const MathEquation = (props) => {
   console.log(props.equations);
+
+ const task = getRandomElement(props.equations)
+  console.log(task)
   return (
-    <div>aaa</div>
+    <div>{task.equation}</div>
   )
-}
+};
 
 export default MathEquation
