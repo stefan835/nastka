12f9ebdf412408a1a782d3c2af79790020ca8494
Piotr Stefanowicz <stefan8357@gmail.com>
2017-10-11 19:05:32 +0200

    MathEquation/ moved answer input to new component, caused bug with input loosing focus

---
+++ nastka-app/app/src/components/Math/EquationInput.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+class Input extends React.Component {
+
+  state = {
+    answer: '',
+  };
+
+  render() {
+    const handleChange = (e) => {
+      this.setState({
+        answer: e.target.value
+      });
+    };
+    return (
+      <input value={this.state.answer}
+             onChange={handleChange}
+             className="equation-result--input no-spin"
+             type="text"/>
+    )
+  }
+}
+
+export default Input
--- nastka-app/app/src/components/Math/MathEquation.js
+++ nastka-app/app/src/components/Math/MathEquation.js
@@ -1,18 +1,16 @@
 import React from 'react'
 
 import getRandomElement from '../../_utils/getRandomElement/getRandomElement'
+import EquationInput from './EquationInput'
+
 
 const MathEquation = (props) => {
 
-  const Input = () => {
-    return <input className="equation-result--input no-spin" type="number"/>
-  };
-
   const task = getRandomElement(props.equations);
   const equation = task.equation.split('').map((character) => {
-    return character === '_' ? <Input/> : character
-  });
+    return character === '_' ? <EquationInput/> : character
 
+  });
   return (
     <div>
       {
